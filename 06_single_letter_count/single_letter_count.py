088386eab1f2a88db359e320c1700a146d5051ad
alexm <alex-m@outlook.com>
2022-12-13 20:48:21 -0800

    Added code for problem 06

--- 06_single_letter_count/single_letter_count.py
+++ 06_single_letter_count/single_letter_count.py
@@ -1,5 +1,6 @@
 def single_letter_count(word, letter):
-    """How many times does letter appear in word (case-insensitively)?
+    """
+    How many times does letter appear in word (case-insensitively)?
     
         >>> single_letter_count('Hello World', 'h')
         1
@@ -10,3 +11,5 @@ def single_letter_count(word, letter):
         >>> single_letter_count("Hello World", 'l')
         3
     """
+
+    return word.lower().count(letter.lower())
